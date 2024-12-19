07a618bc554b3571032cf5f0a79317e33df9f923
alamarnissi <ala.marnissi30@gmail.com>
2024-12-19 13:32:28 +0100

    Add ability to test on Desired Browser/Cross Browser/Parallel

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -2,7 +2,25 @@ import pytest
 from selenium import webdriver
 
 @pytest.fixture()
-def setup():
-    driver = webdriver.Firefox()
+def setup(browser):
+    if browser == "chrome":  
+        driver = webdriver.Chrome()
+        print("launching Chrome browser....")
+    elif browser == "firefox":
+        driver = webdriver.Firefox()
+        print("launching Firefox browser....")
+    else:
+        driver = webdriver.Edge()
+
     driver.maximize_window()
     return driver
+
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+
+@pytest.fixture()
+def browser(request):
+    return request.config.getoption("--browser")
+
+
+
